e7f0426d45a1c07896b207e25e28dd49bd8537ca
nuttyinc <calebgamerpro9999@gmail.com>
2026-09-13 20:42:34 -0400

    Point public downloads to the verified v1.0.4 installer

--- web/landing.js
+++ web/landing.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('#launcher-download');
 const agreement = document.querySelector('#license-check');
-const releaseUrl = 'https://github.com/nuttyinc578/download.net/releases/download/v1.0.3/download.net-Setup-1.0.3.exe';
+const releaseUrl = 'https://github.com/nuttyinc578/download.net/releases/download/v1.0.4/download.net-Setup-1.0.4.exe';
 
 const updateDownload = () => { button.disabled = !agreement.checked; };
 agreement.addEventListener('change', updateDownload);
